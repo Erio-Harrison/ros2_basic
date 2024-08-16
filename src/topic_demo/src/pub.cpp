5538373dcdbfc9fa3ce6833f31095e200710eede
Erio-Harrison <u7541840@gmail.com>
2024-08-16 10:22:00 +1000

    define own variable for dictionary

--- src/topic_demo/src/pub.cpp
+++ src/topic_demo/src/pub.cpp
@@ -1,59 +1,92 @@
-#include <boost/beast/core.hpp>
-#include <boost/beast/websocket.hpp>
-#include <boost/asio/connect.hpp>
-#include <boost/asio/ip/tcp.hpp>
-#include <cstdlib>
-#include <iostream>
-#include <string>
-#include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
-#include "interfaces_demo/msg/person_info.hpp"
+#include <rclcpp/rclcpp.hpp>
+#include "interfaces_demo/msg/key_value.hpp"
+#include "interfaces_demo/msg/dictionary.hpp"
 
 class Pub : public rclcpp::Node
 {
 public:
     Pub(std::string name) : Node(name)
     {
-        RCLCPP_INFO(this->get_logger(), "node is running.");
-        // 2.Create publisher
-        pub = this->create_publisher<interfaces_demo::msg::PersonInfo>("name", 10);
+        RCLCPP_INFO(this->get_logger(), "Node is running.");
+        // 2. Create publisher
+        pub = this->create_publisher<interfaces_demo::msg::Dictionary>("name", 10);
         // Create timer to publish information
         timer_ = this->create_wall_timer(std::chrono::milliseconds(1000),
                                          std::bind(&Pub::send_msg, this));
     }
 
 private:
-    // 1. Claim publisher
-    rclcpp::Publisher<interfaces_demo::msg::PersonInfo>::SharedPtr pub;
-    // 3.release news
+    // 1. Declare publisher
+    rclcpp::Publisher<interfaces_demo::msg::Dictionary>::SharedPtr pub;
+
+    // 3. Publish messages
     void send_msg()
     { 
+        interfaces_demo::msg::Dictionary dict_msg;
 
-        interfaces_demo::msg::PersonInfo info;
-        // Create message
-        info.symbol = "XBTUSD";
-        info.id = 25585432705;
-        info.side = "Sell";
-        info.timestamp = "2024-03-24T13:50:14.196Z";
-        info.size = 34000;
-        info.price = 65479;
+        // Create first KeyValue pair
+        interfaces_demo::msg::KeyValue kv1;
+        kv1.key = "symbol";
+        kv1.value = "XBTUSD";
+        dict_msg.data.push_back(kv1);
 
-        interfaces_demo::msg::PersonInfo info2;
-        info2.symbol = "ASDFG";
-        info2.id = 1584;
-        info2.side = "sold";
-        info2.timestamp = "2024-03-24T88:50:14.196Z";
-        info2.size = 987456;
-        info2.price = 89522;
-        // Log printing
-        RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", info.symbol.c_str());
-        RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", info2.symbol.c_str());
+        kv1.key = "id";
+        kv1.value = "25585432705";
+        dict_msg.data.push_back(kv1);
 
-        // make an announcement
-        pub->publish(info);
-        pub->publish(info2);
+        kv1.key = "side";
+        kv1.value = "Sell";
+        dict_msg.data.push_back(kv1);
+
+        kv1.key = "timestamp";
+        kv1.value = "2024-03-24T13:50:14.196Z";
+        dict_msg.data.push_back(kv1);
+
+        kv1.key = "size";
+        kv1.value = "34000";
+        dict_msg.data.push_back(kv1);
+
+        kv1.key = "price";
+        kv1.value = "65479";
+        dict_msg.data.push_back(kv1);
+
+        // Log the publishing action
+        RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", kv1.value.c_str());
+
+        // Create second KeyValue pair and add to the dictionary
+        interfaces_demo::msg::KeyValue kv2;
+        kv2.key = "symbol";
+        kv2.value = "ASDFG";
+        dict_msg.data.push_back(kv2);
+
+        kv2.key = "id";
+        kv2.value = "1584";
+        dict_msg.data.push_back(kv2);
+
+        kv2.key = "side";
+        kv2.value = "Sold";
+        dict_msg.data.push_back(kv2);
+
+        kv2.key = "timestamp";
+        kv2.value = "2024-03-24T88:50:14.196Z";
+        dict_msg.data.push_back(kv2);
+
+        kv2.key = "size";
+        kv2.value = "987456";
+        dict_msg.data.push_back(kv2);
+
+        kv2.key = "price";
+        kv2.value = "89522";
+        dict_msg.data.push_back(kv2);
+
+        // Log the publishing action
+        RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", kv2.value.c_str());
+
+        // Publish the dictionary message
+        pub->publish(dict_msg);
     }
-    // 4. declare timer
+
+    // 4. Declare timer
     rclcpp::TimerBase::SharedPtr timer_;
 };
 
--- src/topic_demo/src/sub.cpp
+++ src/topic_demo/src/sub.cpp
@@ -1,32 +1,33 @@
-#include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
-#include "interfaces_demo/msg/person_info.hpp"
+#include <rclcpp/rclcpp.hpp>
+#include "interfaces_demo/msg/key_value.hpp"
+#include "interfaces_demo/msg/dictionary.hpp"
+
 class Sub : public rclcpp::Node
 {
 public:
     Sub(std::string name) : Node(name)
     {
-        RCLCPP_INFO(this->get_logger(), "node is running.");
+        RCLCPP_INFO(this->get_logger(), "Node is running.");
         // 3. Create a subscriber
-        sub = this->create_subscription<interfaces_demo::msg::PersonInfo>("name", 10,
+        sub = this->create_subscription<interfaces_demo::msg::Dictionary>("name", 10,
                 std::bind(&Sub::sub_callback, this, std::placeholders::_1));
     }
 
 private:
-    // 1.Declare subscribers
-    rclcpp::Subscription<interfaces_demo::msg::PersonInfo>::SharedPtr sub;
-    // 2.Subscriber callback function
-    void sub_callback(const interfaces_demo::msg::PersonInfo::SharedPtr msgs)
+    // 1. Declare subscriber
+    rclcpp::Subscription<interfaces_demo::msg::Dictionary>::SharedPtr sub;
+
+    // 2. Subscriber callback function
+    void sub_callback(const interfaces_demo::msg::Dictionary::SharedPtr msg)
     {
-        RCLCPP_INFO(this->get_logger(), "Receiving name: %s, id: %ld, side: %s \n                                     timestamp: %s, size: %ld, price: %ld",
-                    msgs->symbol.c_str(),
-                    msgs->id,
-                    msgs->side.c_str(),
-                    msgs->timestamp.c_str(),
-                    msgs->size,
-                    msgs->price);
+        for (const auto &kv : msg->data)
+        {
+            RCLCPP_INFO(this->get_logger(), "Receiving key: %s, value: %s",
+                        kv.key.c_str(), kv.value.c_str());
+        }
     }
 };
+
 int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
